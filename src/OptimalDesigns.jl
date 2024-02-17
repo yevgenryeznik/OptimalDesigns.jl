7ae31893edad6bbc44db6b1bfa2a672ab37137ad
yevgenryeznik <yevgen.ryeznik@gmail.com>
2024-02-17 20:16:57 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.48

---
+++ src/OptimalDesigns.jl
@@ -0,0 +1,5 @@
+module OptimalDesigns
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OptimalDesigns
+using Test
+
+@testset "OptimalDesigns.jl" begin
+    # Write your tests here.
+end
